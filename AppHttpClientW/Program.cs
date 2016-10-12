3acebfdb12b2fe0abae8f043f5e119bcb6a1ced0
Joel Rivera <Joel Rivera>
2016-10-12 13:36:15 -0400

    First commit, yeah

---
+++ AppHttpClientW/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppHttpClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("-------- INTERNET GREATNESS --------");
+            Console.WriteLine("------------------------------------");
+
+
+
+            string sURL = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=space";
+
+            WebRequest wrGETURL;
+            wrGETURL = WebRequest.Create(sURL);
+
+
+
+
+
+            //WebProxy myProxy = new WebProxy("myproxy", 80);
+            //myProxy.BypassProxyOnLocal = true;
+
+            //wrGETURL.Proxy = myProxy;
+
+
+
+            // Making the request
+            WebResponse response;
+            response = wrGETURL.GetResponseAsync().Result;
+
+            Stream respStream = response.GetResponseStream();
+
+            //Console.WriteLine(respStream);
+
+
+
+
+
+            //Reading and printing data to console -----------------------
+            StreamReader objReader = new StreamReader(respStream);
+
+            string jsonStringResponse = objReader.ReadLine();
+
+            Console.WriteLine( jsonStringResponse );
+
+
+
+
+            //string sLine = "";
+            //int i = 0;
+
+            //while (sLine != null)
+            //{
+            //    i++;
+            //    sLine = objReader.ReadLine();
+
+
+            //    if (sLine != null)
+            //        Console.WriteLine("{0}:{1}", i, sLine);
+            //}
+
+
+
+        }
+    }
+}
---
+++ AppHttpClientW/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AppHttpClientW")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AppHttpClientW")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a0eb6f0a-1514-451f-b779-111b8c2ce91c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
