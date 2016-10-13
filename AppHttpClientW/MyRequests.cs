8ab0f8d7ba61b13eb1d963e87042d81dc74b5b6a
Joel Rivera <tilyxxx@gmail.com>
2016-10-12 21:07:35 -0400

    This refactors some functionality and puts it in their own classes

--- AppHttpClientW/MyRequests.cs
+++ AppHttpClientW/MyRequests.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Serialization;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace AppHttpClientW
 {
-    class MyRequests
+    public static class MyRequests
     {
+        public static JArray WikipediaRequest( string searchString )
+        {
+
+            string sURL = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + searchString;
+
+            WebRequest wrGETURL;
+            wrGETURL = WebRequest.Create(sURL);
+
+
+
+            // Making the request
+            WebResponse response;
+            response = wrGETURL.GetResponseAsync().Result;
+
+            Stream respStream = response.GetResponseStream();
+
+
+            //Reading and printing data to console -----------------------
+            StreamReader objReader = new StreamReader(respStream);
+
+            string jsonStringResponse = objReader.ReadLine();
+
+            var infoObject = (JArray)JsonConvert.DeserializeObject(jsonStringResponse);
+
+            return infoObject;
+        }
     }
 }
--- AppHttpClientW/Program.cs
+++ AppHttpClientW/Program.cs
@@ -17,34 +17,12 @@ namespace AppHttpClientW
             Console.WriteLine("------------------------------------");
             Console.WriteLine("------- INTERNET AWESOMENESS -------");
             Console.WriteLine("------------------------------------");
-            
-            string sURL = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=arcade";
 
-            WebRequest wrGETURL;
-            wrGETURL = WebRequest.Create(sURL);
+            JArray wikiResult;
+            wikiResult =  MyRequests.WikipediaRequest("dogs");
+            Render.WikipediaPrintResults( wikiResult );
 
-            
-
-            // Making the request
-            WebResponse response;
-            response = wrGETURL.GetResponseAsync().Result;
-
-            Stream respStream = response.GetResponseStream();
-            
-
-            //Reading and printing data to console -----------------------
-            StreamReader objReader = new StreamReader(respStream);
-
-            string jsonStringResponse = objReader.ReadLine();
-
-            var infoObject = (JArray)JsonConvert.DeserializeObject( jsonStringResponse );
-
-            Console.WriteLine( infoObject[0] );
-            Console.WriteLine( infoObject[1] );
-            Console.WriteLine( infoObject[2] );
-
-
-            Render.PrintResults();
+           
             
             
         }
