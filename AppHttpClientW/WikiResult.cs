1abee7529944920da1aa82d9bcfd7c8bf598e28b
Joel Rivera <tilyxxx@gmail.com>
2016-10-12 21:26:03 -0400

    Created class 'WikiResult'. Not yet implemented, not yet in use

---
+++ AppHttpClientW/WikiResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace AppHttpClientW
+{
+    class WikiResult
+    {
+        private JArray _data; 
+
+        public WikiResult( JArray data )
+        {
+            _data = data;
+        }
+
+        public void Render()
+        {
+            Console.WriteLine(_data[0]);
+            Console.WriteLine(_data[1]);
+            Console.WriteLine(_data[2]);
+        }
+        //private _data = 
+    }
+}
