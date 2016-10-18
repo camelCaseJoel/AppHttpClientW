1d59d0fb97e593d2167f147b38e6ad94d783624c
Joel Rivera <tilyxxx@gmail.com>
2016-10-18 18:30:26 -0400

    At last!  It's connecting to the database!

--- AppHttpClientW/Program.cs
+++ AppHttpClientW/Program.cs
@@ -39,10 +39,19 @@ namespace AppHttpClientW
             SqlConnection conn = new SqlConnection();
             conn.ConnectionString = "Server=LAPTOP-3NG87PA6;Database=CoolConsoleAppClient;User Id=sa;Password = P@55w0rdP@55w0rd;";
 
+            //debug
+            Console.WriteLine(conn);
+
             conn.Open();
 
+            //debug
+            Console.WriteLine(conn.State);
+
             // use the connection here
-            SqlCommand insertCommand = new SqlCommand("INSERT INTO Searches (name) VALUES (kitty)", conn);
+            SqlCommand insertCommand = new SqlCommand("INSERT INTO Searches (name) VALUES ('from program')", conn);
+
+            var rowsAffected =  insertCommand.ExecuteNonQuery();
+            Console.WriteLine(rowsAffected);
 
             conn.Close();
             conn.Dispose();
